7bd2a8c6289eab7cc9e2c15c37531f2e95c4ece9
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-17 21:56:32 -0700

    state lifting

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 
 function CreateTweet() {
   const [textInput, setTextInput] = useState("");
+  const [tweets, setTweets] = useState([]);
 
   const userInputHandler = (event) => {
     setTextInput(event.target.value);
   };
+  const submitTweetHandler = (e) => {
+    e.preventDefault();
+    setTweets([...tweets, textInput]);
+  };
 
   return (
-    <form>
+    <form onSubmit={submitTweetHandler}>
       <textarea onChange={userInputHandler} cols="40" rows="5"></textarea>
       <button>Submit</button>
     </form>
