3f28d277724d30c71ddc0c97aa3829d16fb748aa
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-17 20:50:24 -0700

    events

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
 
 function App() {
+  function helloBtn(event) {
+    console.log(event.target.classList);
+  }
+
   return (
     <div className="box">
       <h1>Hello React</h1>
-      <CreateTweet />
-      <TweetList />
+      <button onClick={helloBtn}>Click</button>
     </div>
   );
 }
