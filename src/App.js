a8aed5cbab432c89dd38d1cde3cbe07b25c0eee9
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-17 20:00:22 -0700

    structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function App() {
+  function clickBtn() {
+    alert("Button clicked.");
+  }
   return (
     <div className="box">
       <h1>Hello React</h1>
+      <button onClick={clickBtn}>Click Me</button>
     </div>
   );
 }
