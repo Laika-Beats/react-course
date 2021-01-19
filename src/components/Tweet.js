a63980ea6d7ca23ca9bc7beb934ade90cf8c1f8b
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-18 19:51:30 -0700

    component magic

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function Tweet({ name, message, tweets }) {
+function Tweet({ name, tweet, setTweets, tweets }) {
+  const deleteTweet = () => {
+    setTweets(tweets.filter((t) => t !== tweet));
+  };
   const tweetDate = "01/17/2021";
   return (
     <div className="tweet">
       <h2>{name}</h2>
       <h3>{tweetDate}</h3>
-      <h4>{tweets}</h4>
-      <button>Delete</button>
+      <h4>{tweet}</h4>
+      <button onClick={deleteTweet}>Delete</button>
       <button>Like</button>
     </div>
   );
