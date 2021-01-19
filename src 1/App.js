5f6ce7dc870af09ad199bf70fd30053a0ef77c0b
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-19 16:50:10 -0700

    practice

---
+++ src 1/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import TweetList from "./components/TweetList";
+import CreateTweet from "./components/CreateTweet";
+
+function App() {
+  const name = "Joshua";
+  const [textInput, setTextInput] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    console.log("we run a function");
+  }, []);
+
+  return (
+    <div className="box">
+      <h1>YO CHECK OUT THIS STYLE</h1>
+      <CreateTweet
+        textInput={textInput}
+        setTextInput={setTextInput}
+        tweets={tweets}
+        setTweets={setTweets}
+      />
+      <TweetList name={name} tweets={tweets} setTweets={setTweets} />
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from "react";
-import TweetList from "./components/TweetList";
-import CreateTweet from "./components/CreateTweet";
+import React from "react";
+import CreateNote from "./components/CreateNote";
 
 function App() {
-  const name = "Joshua";
-  const [textInput, setTextInput] = useState("");
-  const [tweets, setTweets] = useState([]);
-
-  useEffect(() => {
-    console.log("we run a function");
-  }, []);
-
   return (
     <div className="box">
       <h1>YO CHECK OUT THIS STYLE</h1>
-      <CreateTweet
-        textInput={textInput}
-        setTextInput={setTextInput}
-        tweets={tweets}
-        setTweets={setTweets}
-      />
-      <TweetList name={name} tweets={tweets} setTweets={setTweets} />
+      <CreateNote />
     </div>
   );
 }
