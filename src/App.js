b239962b5651f76744082c823f61ea4e5ef05891
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-21 20:40:20 -0700

    events

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import TweetList from "./components/TweetList";
 
 function App() {
   const name = "Joshua";
+
+  const sayHelloHandler = (e) => {
+    console.log(e.target.className);
+  };
+
   return (
     <div>
       <h1>hello react</h1>
-      <CreateTweet />
-      <TweetList name={name} message="this is a message i think" />
+      <button className="active" onClick={sayHelloHandler}>
+        Click
+      </button>
+      {/* <CreateTweet />
+      <TweetList name={name} message="this is a message i think" /> */}
     </div>
   );
 }
