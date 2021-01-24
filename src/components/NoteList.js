3e06ebfec0535ee690b816187d9fa58c93969f86
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-23 20:53:38 -0700

    prac

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Note from "./Note";
+import { v4 as uuidv4 } from "uuid";
 
 function NoteList({ user, notes, setNotes }) {
+  const key = () => uuidv4();
+  console.log(key);
   return (
     <div>
       {notes.map((note) => (
-        <Note user={user} note={note} notes={notes} setNotes={setNotes} />
+        <Note
+          user={user}
+          note={note}
+          notes={notes}
+          setNotes={setNotes}
+          key={key}
+        />
       ))}
     </div>
   );
