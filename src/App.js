647ea2a3d4dc60b07df9e49d4fa795e9a76b65ea
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-12 13:13:48 -0700

    practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
 
 function App() {
-  const user = "Joshua";
+  const [user, setUser] = useState("Joshua");
+  const [counter, setCounter] = useState(0);
+  const sayHelloHandler = () => {
+    if (user === "Joshua") {
+      setUser("Laika Beats");
+    } else {
+      setUser("Joshua");
+    }
+    setCounter(counter + 1);
+  };
   return (
     <div>
-      <CreateTweet />
-      <TweetList user={user} />
+      <h1>Hello {user}</h1>
+      <h2>You've clicked {counter} times</h2>
+      <button onClick={sayHelloHandler}>Click</button>
+      {/* <CreateTweet />
+      <TweetList user={user} /> */}
     </div>
   );
 }
