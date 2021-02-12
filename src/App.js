74ef0bce1eb07eb1b64a50208f24e721a2c5a762
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-12 13:34:44 -0700

    practice

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import CreateTweet from "./components/CreateTweet";
 
 function App() {
   const [user, setUser] = useState("Joshua");
-  const [counter, setCounter] = useState(0);
-  const sayHelloHandler = () => {
-    if (user === "Joshua") {
-      setUser("Laika Beats");
-    } else {
-      setUser("Joshua");
-    }
-    setCounter(counter + 1);
-  };
   return (
     <div>
-      <h1>Hello {user}</h1>
-      <h2>You've clicked {counter} times</h2>
-      <button onClick={sayHelloHandler}>Click</button>
-      {/* <CreateTweet />
-      <TweetList user={user} /> */}
+      <CreateTweet />
+      <TweetList user={user} />
     </div>
   );
 }
--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateTweet = () => {
+  const [textInput, setTextInput] = useState("");
+
+  const userInputHandler = (e) => {
+    setTextInput(e.target.value);
+  };
   return (
     <form>
-      <textarea></textarea>
+      <h1>{textInput}</h1>
+      <textarea value={textInput} onChange={userInputHandler}></textarea>
       <button>Submit</button>
     </form>
   );
