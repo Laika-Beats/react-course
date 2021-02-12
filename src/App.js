e252c4f0d1835554ffc3ae5fc7ca36541d57389a
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-12 14:43:46 -0700

    useEffect practice

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
 
 function App() {
-  const [user, setUser] = useState("Joshua");
+  const user = "Joshua";
   const [textInput, setTextInput] = useState("");
   const [tweets, setTweets] = useState([]);
 
