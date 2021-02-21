ca02c149998e94cb2a72c3bf063d492ac95a3b06
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-21 11:45:35 -0700

    practice

--- src/components/CreateNote/CreateNote.js
+++ src/components/CreateNote/CreateNote.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const CreateNote = () => {
+  const [noteInput, setNoteInput] = useState("");
+  const [notes, setNotes] = useState([]);
+
+  const inputValue = (event) => {
+    setNoteInput(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const submitNote = (event) => {
+    event.preventDefault();
+    setNotes([...notes, noteInput]);
+    setNoteInput(" ");
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={submitNote}>
         <h2>Write a Note:</h2>
-        <textarea></textarea>
+        <h3>{notes}</h3>
+        <textarea value={noteInput} onChange={inputValue}></textarea>
         <button>Submit</button>
         <button>Clear</button>
       </form>
