ffcc2c2d7bb913b06285cf13aa39184a90d3ec0c
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-22 21:27:35 -0700

    redux practice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore } from "redux";
+import allReducers from "./reducers";
+
+const store = createStore(allReducers);
 
 ReactDOM.render(
   <React.StrictMode>
