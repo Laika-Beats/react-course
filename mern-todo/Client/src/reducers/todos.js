2662c92e5cc3e4d9aa10a58e434875fb9db09ce5
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-25 21:20:52 -0700

    Reducers. Front-end api complete + connected

---
+++ mern-todo/Client/src/reducers/todos.js
@@ -0,0 +1,16 @@
+export default function todos(todos = [], action) {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return action.payload;
+    case "CREATE":
+      return [...todos, action.payload];
+    case "DELETE":
+      return todos.filter((todo) => todo._id === action.payload);
+    case "UPDATE":
+      return todos.map((todo) =>
+        todo._id === action.payload._id ? action.payload : todo
+      );
+    default:
+      return todos;
+  }
+}
