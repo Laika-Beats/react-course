c8631d020c65b074d8ac17e6e39ac92a3c0c0e89
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-26 11:45:36 -0700

    Create todo working.

--- mern-todo/Client/src/components/CreateTodo/CreateTodo.js
+++ mern-todo/Client/src/components/CreateTodo/CreateTodo.js
@@ -1,9 +1,35 @@
-const CreateTodo = () => {
+import { useEffect } from "react";
+import { createTodo, updateTodo } from "../../actions/todos";
+import { useDispatch, useSelector } from "react-redux";
+
+const CreateTodo = ({ todoData, setTodoData, currentId }) => {
+  const dispatch = useDispatch();
+
+  const todo = useSelector((state) =>
+    currentId ? state.todos.find((t) => t._id === currentId) : null
+  );
+
+  useEffect(() => {
+    if (todo) setTodoData(todo);
+  }, [todo, setTodoData]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (currentId === 0) {
+      dispatch(createTodo({ ...todoData }));
+    } else {
+      dispatch(updateTodo(currentId, { ...todoData }));
+    }
+  };
+
   return (
     <form>
       <h1>Write a todo: </h1>
-      <input></input>
-      <button>Submit</button>
+      <textarea
+        value={todoData.message}
+        onChange={(e) => setTodoData({ ...todoData, message: e.target.value })}
+      ></textarea>
+      <button onClick={submitHandler}>Submit</button>
     </form>
   );
 };
