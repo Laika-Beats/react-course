3ee2163194c1138352e8116fd16f5c7fef9f1034
Laika-Beats <laikabeatsdenver@gmail.com>
2021-03-23 11:14:21 -0600

    text input working

--- practice archive/src/components/CreateTodo.js
+++ practice archive/src/components/CreateTodo.js
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 const CreateTodo = () => {
   const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
   const inputText = (e) => {
     setTodoInput(e.target.value);
   };
 
-  const submitTodoHandler = () => {
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
     console.log(todoInput);
+    setTodos([...todos, todoInput]);
+    setTodoInput("");
   };
 
   return (
