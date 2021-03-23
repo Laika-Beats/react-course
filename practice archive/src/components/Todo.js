be8cc865b7a19a825cd046aa9bb1ae9ee859d422
Laika-Beats <laikabeatsdenver@gmail.com>
2021-03-23 11:36:04 -0600

    date working

--- practice archive/src/components/Todo.js
+++ practice archive/src/components/Todo.js
@@ -1,10 +1,16 @@
 const Todo = ({ todo, todos, setTodos }) => {
+  let newDate = new Date();
+  let date = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  let year = newDate.getFullYear();
+
   const deleteHandler = () => {
-    setTodos(todos.filter((t) => t != todo));
+    setTodos(todos.filter((t) => t !== todo));
   };
 
   return (
     <div>
+      <h4>{`${month}/${date}/${year}`}</h4>
       <h2>{todo}</h2>
       <button onClick={deleteHandler}>Delete</button>
     </div>
